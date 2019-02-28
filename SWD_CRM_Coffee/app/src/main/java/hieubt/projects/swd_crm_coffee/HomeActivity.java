1e8d8e1ead2a53b8c7996c4cd905e454549ecb0e
Zeral <tsukaiyo@gmail.com>
2019-02-28 08:15:32 +0700

    zzzz

--- SWD_CRM_Coffee/app/src/main/java/hieubt/projects/swd_crm_coffee/HomeActivity.java
+++ SWD_CRM_Coffee/app/src/main/java/hieubt/projects/swd_crm_coffee/HomeActivity.java
@@ -1,13 +1,49 @@
 package hieubt.projects.swd_crm_coffee;
 
+import android.graphics.drawable.GradientDrawable;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.Gravity;
+import android.widget.LinearLayout;
+import android.widget.TextView;
 
 public class HomeActivity extends AppCompatActivity {
+
+    private LinearLayout layoutHome;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_home);
+        layoutHome = findViewById(R.id.layoutHome);
+
+        createBrands("The Coffee House");
+        createBrands("StarBuck");
+        createBrands("Quang Trung Coffee");
+    }
+
+    private void createBrands(String label){
+        TextView textView = new TextView(this);
+
+        GradientDrawable gd = new GradientDrawable(GradientDrawable.Orientation.TOP_BOTTOM,
+                new int[]{
+                        getResources().getColor(R.color.blueLight100),
+                        getResources().getColor(R.color.blueLight200),
+                        getResources().getColor(R.color.blueLight300)
+                });
+        gd.setCornerRadius(15f);
+        textView.setBackgroundDrawable(gd);
+
+        LinearLayout.LayoutParams layoutParams = new LinearLayout.LayoutParams(LinearLayout.LayoutParams.MATCH_PARENT, 160);
+        layoutParams.leftMargin = 10;
+        layoutParams.rightMargin = 10;
+        layoutParams.topMargin = 5;
+        layoutParams.bottomMargin = 5;
+        textView.setLayoutParams(layoutParams);
+
+        textView.setText(label);
+        textView.setGravity(Gravity.CENTER);
+        textView.setTextColor(getResources().getColor(R.color.black));
+        layoutHome.addView(textView);
     }
 }
--- SWD_CRM_Coffee/app/src/main/java/hieubt/projects/swd_crm_coffee/NotificationActivity.java
+++ SWD_CRM_Coffee/app/src/main/java/hieubt/projects/swd_crm_coffee/NotificationActivity.java
@@ -1,13 +1,44 @@
 package hieubt.projects.swd_crm_coffee;
 
+import android.app.ActionBar;
+import android.graphics.Color;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.Gravity;
+import android.widget.Gallery;
+import android.widget.LinearLayout;
+import android.widget.TextView;
 
 public class NotificationActivity extends AppCompatActivity {
+
+    private LinearLayout layoutNoti;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_notification);
+        layoutNoti = findViewById(R.id.layoutNoti);
+
+        createNotification("50% Discount to Coffee House's product");
+        createNotification("50% Discount to StarBuck's product");
+        createNotification("70% Discount to Coffee House's product");
+        createNotification("Event : Halloween at Coffee House");
+        createNotification("Event : Halloween at StarBuck");
+        createNotification("Mr. Coffee's 1st day opening sale");
+        createNotification("50% Discount to Coffee House's product");
+    }
+
+    private void createNotification(String label){
+        TextView textView = new TextView(this);
+        LinearLayout.LayoutParams layoutParams = new LinearLayout.LayoutParams(LinearLayout.LayoutParams.MATCH_PARENT, 160);
+        layoutParams.leftMargin = 10;
+        layoutParams.rightMargin = 10;
+        layoutParams.topMargin = 5;
+        layoutParams.bottomMargin = 5;
+        textView.setLayoutParams(layoutParams);
+        textView.setBackgroundColor(getResources().getColor(R.color.blueLightA400));
+        textView.setText(label);
+        textView.setGravity(Gravity.CENTER);
+        layoutNoti.addView(textView);
     }
 }

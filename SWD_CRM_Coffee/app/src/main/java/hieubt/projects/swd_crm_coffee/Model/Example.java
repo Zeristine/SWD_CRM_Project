c9d30886a39838702bd44712734827154dad611a
huylm <huylm@dfk-com.vn>
2019-03-06 10:33:33 +0700

    b

---
+++ SWD_CRM_Coffee/app/src/main/java/hieubt/projects/swd_crm_coffee/Model/Example.java
@@ -0,0 +1,65 @@
+package hieubt.projects.swd_crm_coffee.Model;
+
+import java.util.List;
+import com.google.gson.annotations.Expose;
+import com.google.gson.annotations.SerializedName;
+
+public class Example {
+
+    @SerializedName("result-code")
+    @Expose
+    private Integer resultCode;
+    @SerializedName("message")
+    @Expose
+    private String message;
+    @SerializedName("success")
+    @Expose
+    private Boolean success;
+    @SerializedName("error")
+    @Expose
+    private String error;
+    @SerializedName("data")
+    @Expose
+    private List<Datum> data = null;
+
+    public Integer getResultCode() {
+        return resultCode;
+    }
+
+    public void setResultCode(Integer resultCode) {
+        this.resultCode = resultCode;
+    }
+
+    public String getMessage() {
+        return message;
+    }
+
+    public void setMessage(String message) {
+        this.message = message;
+    }
+
+    public Boolean getSuccess() {
+        return success;
+    }
+
+    public void setSuccess(Boolean success) {
+        this.success = success;
+    }
+
+    public String getError() {
+        return error;
+    }
+
+    public void setError(String error) {
+        this.error = error;
+    }
+
+    public List<Datum> getData() {
+        return data;
+    }
+
+    public void setData(List<Datum> data) {
+        this.data = data;
+    }
+
+}

336619dd0bbfe06920a48e8659e71e0b41e74d45
ha trieu kim <hatrieukim@gmail.com>
2019-03-07 16:18:37 +0700

    add SQLite, add registed brand data to homeActivity

---
+++ SWD_CRM_Coffee/app/src/main/java/hieubt/projects/swd_crm_coffee/DBManager.java
@@ -0,0 +1,94 @@
+package hieubt.projects.swd_crm_coffee;
+
+import android.content.ContentValues;
+import android.content.Context;
+import android.database.Cursor;
+import android.database.sqlite.SQLiteDatabase;
+import android.database.sqlite.SQLiteOpenHelper;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class DBManager extends SQLiteOpenHelper {
+
+    private Context mContext;
+
+    public DBManager(Context context){
+        super(context, "SWS_database", null, 1);
+        this.mContext= context;
+    }
+    @Override
+    public void onCreate(SQLiteDatabase db) {
+        String sqlQuery = "CREATE TABLE \"registedBrand_table\" (\n" +
+                "\t\"userId\"\tINTEGER,\n" +
+                "\t\"brandId\"\tINTEGER\n" +
+                ")";
+        db.execSQL(sqlQuery);
+        sqlQuery = "CREATE TABLE \"user_table\" (\n" +
+                "\t\"id\"\tINTEGER NOT NULL PRIMARY KEY AUTOINCREMENT,\n" +
+                "\t\"username\"\tINTEGER NOT NULL,\n" +
+                "\t\"password\"\tINTEGER NOT NULL\n" +
+                ")";
+        db.execSQL(sqlQuery);
+    }
+    @Override
+    public void onUpgrade(SQLiteDatabase db, int oldVersion, int newVersion) {
+
+    }
+
+    //login
+    public boolean checkLogin(String username, String password) {
+//        String query = "select * from user_table where username = bbb and password = bbb";
+        SQLiteDatabase db = this.getReadableDatabase();
+        String[] columns = new String[]{"username", "password"};
+        String whereClause = "username = ? and password = ?";
+        String[] whereArgs = new String[]{username, password};
+        Cursor cursor = db.query("user_table", columns, whereClause, whereArgs, null, null, null);
+
+        boolean result = cursor.moveToFirst();
+        cursor.close();
+        db.close();
+        return result;
+    }
+
+    public boolean registed(String username, String password) {
+        ContentValues values = new ContentValues();
+        values.put("username", username);
+        values.put("password", password);
+        SQLiteDatabase db = this.getWritableDatabase();
+        boolean result = db.insert("user_table", null, values) > 0;
+        db.close();
+        return result;
+    }
+
+    //registedBrand table
+    //return list of registed brand id
+    public List<Integer> getAllRegistedBrandId(int userId) {
+        List<Integer> list = new ArrayList<>();
+        String query = "SELECT brandId FROM user_table JOIN registedBrand_table on user_table.id = registedBrand_table.userId WHERE user_table.id = " + String.valueOf(userId);
+        SQLiteDatabase db = this.getReadableDatabase();
+        Cursor cursor = db.rawQuery(query, null);
+        if (cursor.moveToFirst()) {
+            do {
+                list.add(cursor.getInt(0));
+            } while (cursor.moveToNext());
+        }
+        cursor.close();
+        db.close();
+        return list;
+    }
+
+    //regist a brand
+    public boolean addRegistedBrandId(int userId, int brandId) {
+        boolean result = false;
+        ContentValues values = new ContentValues();
+        values.put("userId", userId);
+        values.put("brandId", brandId);
+        SQLiteDatabase db = this.getReadableDatabase();
+        result = db.insert("registedBrand_table", null, values) > 0;
+        db.close();
+
+        return result;
+    }
+
+}
--- SWD_CRM_Coffee/app/src/main/java/hieubt/projects/swd_crm_coffee/HomeActivity.java
+++ SWD_CRM_Coffee/app/src/main/java/hieubt/projects/swd_crm_coffee/HomeActivity.java
@@ -1,11 +1,23 @@
 package hieubt.projects.swd_crm_coffee;
 
+import android.content.Intent;
 import android.graphics.drawable.GradientDrawable;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.Gravity;
 import android.widget.LinearLayout;
 import android.widget.TextView;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import hieubt.projects.swd_crm_coffee.Model.Datum;
+import hieubt.projects.swd_crm_coffee.Model.Example;
+import hieubt.projects.swd_crm_coffee.retrofit.BrandApiClient;
+import hieubt.projects.swd_crm_coffee.retrofit.BrandApiInterface;
+import retrofit2.Call;
+import retrofit2.Callback;
+import retrofit2.Response;
 
 public class HomeActivity extends AppCompatActivity {
 
@@ -20,6 +32,30 @@ public class HomeActivity extends AppCompatActivity {
         createBrands("The Coffee House");
         createBrands("StarBuck");
         createBrands("Quang Trung Coffee");
+
+        //////////////////////////////////////////
+        //show all registed brand
+        //get list registed brand
+        DBManager dbManager = new DBManager(this);
+        List<Integer> listBrandId = dbManager.getAllRegistedBrandId(1);
+        //call api with each brand id
+        final List<Datum> listRegistedBrand = new ArrayList<>();
+        BrandApiInterface service = BrandApiClient.getClient().create(BrandApiInterface.class);
+        for (Integer id : listBrandId) {
+            Call<Example> call = service.getBrandById(id);
+            call.enqueue(new Callback<Example>() {
+                @Override
+                public void onResponse(Call<Example> call, Response<Example> response) {
+                    Datum data = response.body().getData();
+                    listRegistedBrand.add(data);
+                }
+
+                @Override
+                public void onFailure(Call<Example> call, Throwable t) {
+                    System.out.println("FAIL");
+                }
+            });
+        }
     }
 
     private void createBrands(String label){

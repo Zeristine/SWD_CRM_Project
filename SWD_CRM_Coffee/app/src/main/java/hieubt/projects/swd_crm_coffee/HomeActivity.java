4b895bf3ea29c0bbe1931efc7fc7393670f67780
Zeral <tsukaiyo@gmail.com>
2019-03-08 08:31:23 +0700

    create item generator

--- SWD_CRM_Coffee/app/src/main/java/hieubt/projects/swd_crm_coffee/HomeActivity.java
+++ SWD_CRM_Coffee/app/src/main/java/hieubt/projects/swd_crm_coffee/HomeActivity.java
@@ -15,6 +15,7 @@ import hieubt.projects.swd_crm_coffee.Model.Datum;
 import hieubt.projects.swd_crm_coffee.Model.Example;
 import hieubt.projects.swd_crm_coffee.retrofit.BrandApiClient;
 import hieubt.projects.swd_crm_coffee.retrofit.BrandApiInterface;
+import hieubt.projects.swd_crm_coffee.ultilities.ItemGenerator;
 import retrofit2.Call;
 import retrofit2.Callback;
 import retrofit2.Response;
@@ -22,6 +23,7 @@ import retrofit2.Response;
 public class HomeActivity extends AppCompatActivity {
 
     private LinearLayout layoutHome;
+    private final ItemGenerator itemGenerator = new ItemGenerator(this);
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -29,9 +31,9 @@ public class HomeActivity extends AppCompatActivity {
         setContentView(R.layout.activity_home);
         layoutHome = findViewById(R.id.layoutHome);
 
-        createBrands("The Coffee House");
-        createBrands("StarBuck");
-        createBrands("Quang Trung Coffee");
+        itemGenerator.createRectangleWithLabel("The Coffee House", layoutHome);
+        itemGenerator.createRectangleWithLabel("StarBuck", layoutHome);
+        itemGenerator.createRectangleWithLabel("Quang Trung Coffee", layoutHome);
 
         //////////////////////////////////////////
         //show all registed brand
@@ -56,30 +58,5 @@ public class HomeActivity extends AppCompatActivity {
                 }
             });
         }
-    }
-
-    private void createBrands(String label){
-        TextView textView = new TextView(this);
-
-        GradientDrawable gd = new GradientDrawable(GradientDrawable.Orientation.TOP_BOTTOM,
-                new int[]{
-                        getResources().getColor(R.color.blueLight100),
-                        getResources().getColor(R.color.blueLight200),
-                        getResources().getColor(R.color.blueLight300)
-                });
-        gd.setCornerRadius(15f);
-        textView.setBackgroundDrawable(gd);
-
-        LinearLayout.LayoutParams layoutParams = new LinearLayout.LayoutParams(LinearLayout.LayoutParams.MATCH_PARENT, 160);
-        layoutParams.leftMargin = 10;
-        layoutParams.rightMargin = 10;
-        layoutParams.topMargin = 5;
-        layoutParams.bottomMargin = 5;
-        textView.setLayoutParams(layoutParams);
-
-        textView.setText(label);
-        textView.setGravity(Gravity.CENTER);
-        textView.setTextColor(getResources().getColor(R.color.black));
-        layoutHome.addView(textView);
     }
 }
